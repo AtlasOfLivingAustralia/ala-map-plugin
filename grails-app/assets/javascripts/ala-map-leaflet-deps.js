31483f2dc34dfddaed95bcb55fc735fd61ec17fe
temi <temi@users.noreply.github.com>
2026-04-02 16:42:46 +1100

    updated path to easy button js file

--- grails-app/assets/javascripts/ala-map-leaflet-deps.js
+++ grails-app/assets/javascripts/ala-map-leaflet-deps.js
@@ -6,7 +6,7 @@ Dependencies excluding jquery and underscore, includes:
 
 //= require leaflet-draw/leaflet.draw.js
 //= require leaflet.coordinates/Leaflet.Coordinates-0.1.5.src.js
-//= require leaflet-easybutton/easy-button.js
+//= require leaflet-easybutton/src/easy-button.js
 //= require leaflet-control-geocoder/Control.Geocoder.js
 //= require leaflet.markercluster/leaflet.markercluster.js
 //= require leaflet-loading/src/Control.Loading.js
