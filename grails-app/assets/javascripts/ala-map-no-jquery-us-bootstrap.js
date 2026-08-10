dd72d2b8cc24fe1e2b623431a49a1434e2bf21a6
temi <temi@users.noreply.github.com>
2026-08-10 14:10:44 +1000

    - add a manifest without bootstrap dependency

---
+++ grails-app/assets/javascripts/ala-map-no-jquery-us-bootstrap.js
@@ -0,0 +1,8 @@
+//= require underscore/underscore-min.js
+//= require uuidjs/uuid.min.js
+//= require ala-map-jquery-plugins
+//= require ala-map-leaflet
+//= require ala-map-leaflet-deps
+//= require ala-map-handlebars
+//= require ala-map-urijs
+//= require ala-map-no-deps
